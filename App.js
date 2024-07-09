4e64028f9010f11bbcc371f9aa93f52f3821d764
jeons <etoile929@naver.com>
2024-07-09 18:05:46 +0900

    텍스트 및 이미지 컴포넌트

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Typography } from './src/components/Typography';
+import { LocalImage } from './src/components/LocalImage';
+import { RemoteImage } from './src/components/RemoteImage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+          <Typography color="red" fontSize={20}>
+              이것은 텍스트 입니다.
+          </Typography>
+
+          <LocalImage 
+              localAsset = {require('./assets/favicon.png')} 
+              width = {50}
+              height = {50}
+        />
+
+        <RemoteImage
+            url={'https://cafeptthumb-phinf.pstatic.net/20160320_271/toeicking990_1458482400384vdCfb_JPEG/740_IMG_20160320_224304.jpg?type=w800'}
+            width={200}
+            height={100}
+        />
+          
     </View>
   );
 }
